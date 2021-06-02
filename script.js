71e422ac69f7a9cc900d3665b51d98843600e3f7
ash12-coder <ashish.agrawal_cs18@gla.ac.in>
2021-06-02 20:00:13 +0800

    1 file modified

--- script.js
+++ script.js
@@ -2,5 +2,5 @@ let times = new Date();
 let h = times.getHours();
 let m = times.getMinutes();
 
-document.getElementById('hour').innerHTML = h + ':' + m;
-document.getElementById('time').innerHTML = h + ':' + m;
+document.getElementById('hour').innerHTML = `${h}:${m}`;
+document.getElementById('time').innerHTML = `${h}:${m}`;
